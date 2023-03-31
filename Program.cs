8743ae0b9644b37c45aec51c07e5b603159ed4de
7vanael <129544762+7vanael@users.noreply.github.com>
2023-03-31 11:19:34 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Roomba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cleaning a grid of N x M,
+            //represented by array R consisting of N strings, which are M-1 long
+            //Robot starts in top left corner, facing right. Moves forward, turns right if blocked.
+            // '.' = an empty square
+            // 'X' = Occupied, or blocked square
+            string[] room = {"....X..X", "X....X..", "...X.X.." };
+            var vacuum = new Solution();
+            var unitsCleaned = vacuum.solution(room);
+
+            
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roomba
+{
+    internal class Solution
+    {
+        public int solution(string[] R)
+        {
+            return 0;
+        }
+    }
+}
